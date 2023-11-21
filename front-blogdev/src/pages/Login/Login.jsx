71a5627969d53e546632d02531be411f130cfe52
Vitor Stankevicius <118496796+StankeHaseo@users.noreply.github.com>
2023-11-21 12:26:10 -0300

    Aula 2111, auth context

--- front-blogdev/src/pages/Login/Login.jsx
+++ front-blogdev/src/pages/Login/Login.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { userAuthentication } from '../hooks/userAuthentication'; // Importe o objeto 'auth' do Firebase
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      // O usuário foi autenticado com sucesso
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Senha:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={handleLogin}>
+          Entrar
+        </button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
